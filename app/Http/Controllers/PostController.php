8b7451446a791d72df40242685f9510150beb882
Miko2495 <n037pybn125qio@i.softbank.jp>
2022-05-28 10:41:50 +0900

    一覧画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,8 +11,9 @@ class Postcontroller extends Controller
     // index関数の定義
     public function index(Post $post)
     {
-        return $post->get();
-        // $postはPostクラスのインスタンス
+        return view('posts/index')->with(['posts'=>$post->getPaginateByLimit()]);
+        // Bladeファイルがどこの直下にあるか(今回はpostsフォルダの下)
+        // withメソッドでビューと一緒に渡すデータを定義している(変数名=>値)
     }
     //
 }
